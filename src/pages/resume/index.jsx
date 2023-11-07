7e271a4e811d858cfe0ed77ec140abf7895b59f7
Marvin Berrio <marvinccp@gmail.com>
2023-11-07 13:14:36 +0100

    create resumeReducer, update resume styles

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,45 +1,74 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import styles from "../../styles/Resume.module.css";
 import GameLayout from "@/layouts/GameLayout";
 import Link from "next/link";
-import { Points } from "@/components/Points";
 import Image from "next/image";
 import { Press_Start_2P } from "next/font/google";
 const start = Press_Start_2P({ subsets: ["latin"], weight: "400" });
 
 const Index = ({ children }) => {
-  const [points, setPoints] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const [questionsNumber, setQuestionsNumber] = useState([]);
+
+  const initialState = {
+    points: 0,
+    correct: 0,
+    questionsNumber: [],
+  };
+
+  console.log(initialState.points)
+  const gameResumeReducer = (state, action) => {
+    switch (action.type) {
+      case "RESUME":
+        return {
+          ...state,
+          points: action.payload.points,
+          correct: action.payload.correct,
+          questionsNumber: action.payload.questions,
+        };
+    }
+  };
+
+  const [state, dispatch] = useReducer(gameResumeReducer, initialState);
 
   useEffect(() => {
     const points = JSON.parse(localStorage.getItem("points"));
-    setPoints(points.points);
-    setCorrect(points.correct);
-    setQuestionsNumber(points.questions);
-  }, [points]);
-  console.log(points);
-  let reward;
+    if (points) {
+      dispatch({ type: "RESUME", payload: points });
+    }
+  }, [state.points]);
 
-  points === 0
-    ? (reward = "zombi")
-    : points > 0 && points <= 10
-    ? (reward = "good")
-    : points > 10 && points <= 20
-    ? (reward = "excelent")
-    : points > 20 && points <= 30
-    ? (reward = "superior")
-    : points > 30 && points <= 40
-    ? (reward = "god")
-    : points > 40 && points <= 50
-    ? (reward = "heroe")
-    : points > 50
-    ? (reward = "master")
-    : "";
+  //Refactor
+  //se crea una matriz donde esten los rangos y la calificación
 
-  
+  const rewardMap = {
+    0: "zombi",
+    1: "good",
+    11: "excellent",
+    21: "superior",
+    31: "god",
+    41: "heroe",
+    51: "master",
+  };
 
-  console.log(reward);
+  //se crea un array con los valores maximos de los rangos, que son las key
+  //  de las propiedades.
+
+  const rewardKeys = Object.keys(rewardMap);
+
+  /*
+esta función recorre el array y compara el valor  de key actual
+con la siguiente y key y con los puntos. basicamente, evalua si 
+los puntos son menores que la próxima key . . .
+*/
+  // let reward;
+  let reward = rewardKeys.find((key, i) => {
+    const nextKey = rewardKeys[i + 1];
+    return state.points >= key && (nextKey === undefined || state.points < nextKey);
+  });
+
+  //asiganamos una variable que contiene el valor de la key
+
+  let rewardInfo = rewardMap[reward];
+
   const pointsReward = {
     good: {
       img: "/icons/good.png",
@@ -59,30 +88,47 @@ const Index = ({ children }) => {
   return (
     <>
       <GameLayout title="Resumen" />
-      <main className={styles.container}>
-        {children}
-        <div className={styles.resume}>
-          <h2
-            className={start.className}
-            style={{ color: "black", fontSize: "1.5rem", lineHeight: "2rem" }}
-          >{`${correct} Correct of ${questionsNumber.length} Questions`}</h2>
-          <h2
-            style={{ color: "green", fontSize: "1.2rem" }}
-            className={start.className}
-          >{`You have ${points} points`}</h2>
-        </div>
-        <h1>{pointsReward[reward]?.text}</h1>
-        <Image
-          src={pointsReward[reward]?.img}
-          width={200}
-          height={200}
-          alt="reward"
-          className={styles.image}
-        />
-        <Link className={styles.link} href={"/"}>
-          <button className={styles.new_game_button}>New Game</button>
-        </Link>
-      </main>
+
+      {state.points != 0 ? (
+        <main className={styles.container}>
+          {children}
+          <div className={styles.resume}>
+            <h2
+              className={start.className}
+              style={{ color: "black", fontSize: "1.5rem", lineHeight: "2rem" }}
+            >{`${state.correct} Correct of ${state.questionsNumber.length} Questions`}</h2>
+            <h2
+              style={{ color: "green", fontSize: "1.2rem" }}
+              className={start.className}
+            >{`You have ${state.points} points`}</h2>
+          </div>
+          <h1>{pointsReward[rewardInfo]?.text}</h1>
+          <Image
+            src={pointsReward[rewardInfo]?.img}
+            width={200}
+            height={200}
+            alt="reward"
+            className={styles.image}
+          />
+          <Link className={styles.link} href={"/"}>
+            <button className={styles.new_game_button}>New Game</button>
+          </Link>
+        </main>
+      ) : (
+        <section className={styles.zombie}>
+          <h1>{pointsReward[rewardInfo]?.text}</h1>
+          <Image
+            src={pointsReward[rewardInfo]?.img}
+            width={200}
+            height={200}
+            alt="reward"
+            className={styles.image}
+          />
+          <Link className={styles.link} href={"/"}>
+            <button className={styles.new_game_button}>New Game</button>
+          </Link>
+        </section>
+      )}
     </>
   );
 };
